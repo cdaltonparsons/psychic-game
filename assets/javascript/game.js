8e6e8498440268e50ff6d408a01b8b128528d5b2
Charlie Parsons <cdaltonparsons@gmail.com>
2019-05-31 12:44:01 -0600

    tweaked the JS to reset the computer guess with each reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,27 @@
-// Variables //
+
 var wins = 0;
 var losses = 0;
 var guesses = 10;
-// Possible computer and user choices //
+
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z" ];
 var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
+var userGuesses = [];
 
-// references to text on the HTML that we will be overwriting //
-var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesText = document.getElementById("guesses-left");
 var wrongGuesses = document.getElementById("wrong-guesses");
-
+var computerLetter = cpuGuess();
 winsText.textContent = wins;
 lossesText.textContent = losses
 
-// var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-// function and any loops //
+
+
+function cpuGuess(){
+    return computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+}
 
 function reset(){
     guesses = 10;
@@ -27,8 +29,12 @@ function reset(){
     guessesText.textContent = guesses;
     winsText.textContent = wins;
     lossesText.textContent = losses
+    userGuesses = [];
+    cpuGuess();
 
 }
+
+
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
@@ -36,15 +42,18 @@ document.onkeyup = function(event) {
     console.log(computerLetter);
     console.log(userGuess)
 
-    if(userChoices.indexOf(userGuess) > -1) {
+    if(userChoices.indexOf(userGuess) > -1 && (userGuesses.indexOf(userGuess)) < 0) {
         if(guesses > 1) {
         if(userGuess === computerLetter) {
             wins++;
             reset();
             return alert("Congratulations! Press any letter key to begin a new game!")
             
-        } else {
+        } 
+        else {
             guesses--;
+            userGuesses.push(userGuess);
+            console.log(userGuesses)
             
         }
     } else {
